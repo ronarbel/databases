b89f4dd4ad80bd29bbf9378f3436c75388158fec
Hack Reactor Student <student@hackreactor.com>
2018-12-05 21:15:16 -0800

    some changes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,15 +3,37 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
-      //model get function
+      console.log('getting');
+      if(req.type === 'GET'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(models.messages.get());
+      }
     }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function (req, res) {
+      console.log('posting');
+      if(req.type === 'POST'){
+        var result = models.messages.post(req);
+        if (!result){
+          res.writeHead(201);
+          res.end();
+        } else {
+        //should return error or success case
+          res.writeHead(400);
+          res.end(result);
+        }
+      }
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      console.log('userget');
+    },
+    post: function (req, res) {
+      console.log('userpost');
+    }
   }
 };
+
 
--- server/models/index.js
+++ server/models/index.js
@@ -3,29 +3,66 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {
-      var messageArr = [];
       connection.connect(function(err) {
         if (err) {
           console.error('Error connecting: ' + err.stack);
           return;
         }
 
-      console.log('Connected as id ' + connection.threadId);
+        console.log('Connected as id ' + connection.threadId);
       });
 
       connection.query('SELECT * FROM messages', function (error, results, fields) {
         if (error){
           throw error;
         }
-        results.forEach(result => {
-        messageArr.push(result);
-        console.log(result); //check structure and see if there are any extra procedures needed to convert into JSON string
+        console.log(results); //check structure and see if there are any extra procedures needed to convert into JSON string);
+        return JSON.stringify(results);
       });
-    });
- //convert to sql js syntax
-      //parse into actual message objects
     }, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function () {
+      let body = "";
+      req.on('data', (chunk) => {
+        body += chunk;
+      }).on('end', () => {
+        connection.connect(function(err) {
+          if (err) {
+            console.error('Error connecting: ' + err.stack);
+            return (err);
+          }
+          
+          console.log('Connected as id ' + connection.threadId);
+        });
+        // set insert ID of the roomname and username entry for messages foreign key reference
+        var username_id = 0;
+        var roomname_id = 0;
+        var message = JSON.parse(body);
+        //create username entry
+        connection.query('INSERT INTO username (name) VALUES (' + message.username + ')' , function(err, result) {
+          if (err){
+            throw (err);
+          }
+          console.log('username entry created');
+          username_id = result.insertId;
+        });
+        //create roomname entry
+        connection.query('INSERT INTO roomname (name) VALUES (' + message.roomname + ')' , function(err, result) {
+          if (err){
+            throw(err);
+          }
+          console.log('roomname entry created');
+          roomname_id = result.insertId;
+        });
+        //create messages entry
+        connection.query(`INSERT INTO messages (username_id, roomname_id, text) VALUES (${username_id}, ${roomname_id}, ${message.text})`, function(err, result) {
+          if (err){
+            throw(err);
+          }
+          console.log('messages entry created');
+        });
+        return;
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
